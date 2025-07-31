14bd12c7adedda3010a962ec4d437a39d11f6f45
Triumphant307 <usuangbontriumphant@gmail.com>
2025-07-31 22:55:47 +0100

    refactored LottieAnimation to use typescript

--- app/components/Home/LottieAniamtion.tsx
+++ app/components/Home/LottieAniamtion.tsx
@@ -1,7 +1,8 @@
 import Lottie from "lottie-react";
 import animationData from "@/app/assets/lotties/Animation.json";
+import React from "react";
 
-const LottieAnimation = () => {
+const LottieAnimation : React.FC = () => {
   return (
     <div style={{ maxWidth: 400, margin: "0 auto" }}>
       <Lottie animationData={animationData} loop={true} />
