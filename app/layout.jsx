6dd5e6340f2da8c42502815ada15e7ac68da6ad5
Triumphant307 <usuangbontriumphant@gmail.com>
2025-09-06 09:00:49 +0100

    Added progress bar when change pages routing

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,11 @@
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
 import BackToTop from "@/app/components/BackToTop";
+import NextTopLoader from "nextjs-toploader";
 import { HabitProvider } from "../app/context/HabitContext";
-
 
 import "./globals.css";
 
@@ -19,23 +18,30 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <NextTopLoader
+          color="#22c55e;"
+          height={3}
+          showSpinner={false}
+          speed={500}
+          easing="ease"
+        />
         <HabitProvider>
-        <Header />
-        <main>
-          <ToastContainer
-            position="top-right"
-            autoClose={2000}
-            hideProgressBar={false}
-            closeOnClick
-            pauseOnHover
-            draggable
-            limit={2}
-            containerClassName="custom-toast-container"
-          />
-          {children}
-        </main>
-        <Footer />
-        <BackToTop />
+          <Header />
+          <main>
+            <ToastContainer
+              position="top-right"
+              autoClose={2000}
+              hideProgressBar={false}
+              closeOnClick
+              pauseOnHover
+              draggable
+              limit={2}
+              containerClassName="custom-toast-container"
+            />
+            {children}
+          </main>
+          <Footer />
+          <BackToTop />
         </HabitProvider>
       </body>
     </html>
