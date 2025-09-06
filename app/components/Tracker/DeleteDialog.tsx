1e7c90a91e62b44b600906243ea9f7329963f67e
Triumphant307 <usuangbontriumphant@gmail.com>
2025-09-06 02:47:45 +0100

    Added animation to the delete dialog in the tracker page

--- app/components/Tracker/DeleteDialog.tsx
+++ app/components/Tracker/DeleteDialog.tsx
@@ -1,13 +1,20 @@
 import { useRef, useEffect } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import styles from "../../Styles/Tracker/DeleteDialog.module.css";
 
-interface DeleteDialogProp  {
+interface DeleteDialogProp {
   isOpen: boolean;
   onClose: () => void;
   onConfirm: () => void;
-};
+}
 
-const DeleteDialog: React.FC<DeleteDialogProp> = ({ isOpen, onClose, onConfirm }) => {
+const MotionDialog = motion.dialog;
+
+const DeleteDialog: React.FC<DeleteDialogProp> = ({
+  isOpen,
+  onClose,
+  onConfirm,
+}) => {
   const dialogRef = useRef<HTMLDialogElement>(null);
 
   useEffect(() => {
@@ -24,34 +31,42 @@ const DeleteDialog: React.FC<DeleteDialogProp> = ({ isOpen, onClose, onConfirm }
   }, [isOpen]);
 
   return (
-    <dialog
-      className={styles.dialog}
-      ref={dialogRef}
-      onClose={onClose}
-      onCancel={onClose}
-    >
-      <h2>Delete Habit</h2>
-      <p>
-        Are you sure you want to delete this habit? This action cannot be
-        undone.
-      </p>
-      <div className={styles.dialogActions}>
-        <button
-          onClick={onClose}
-          className={styles.cancelBtn}
-          title="Cancel habit"
+    <AnimatePresence>
+      {isOpen && (
+        <MotionDialog
+          className={styles.dialog}
+          ref={dialogRef}
+          onClose={onClose}
+          onCancel={onClose}
+          initial={{ opacity: 0, scale: 0.8, y: -30 }}
+          animate={{ opacity: 1, scale: 1, y: 0 }}
+          exit={{ opacity: 0, scale: 0.8, y: 30 }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
         >
-          Cancel
-        </button>
-        <button
-          className={styles.deleteBtn}
-          onClick={onConfirm}
-          title="Delete habit"
-        >
-          Delete
-        </button>
-      </div>
-    </dialog>
+          <h2>Delete Habit</h2>
+          <p>
+            Are you sure you want to delete this habit? This action cannot be
+            undone.
+          </p>
+          <div className={styles.dialogActions}>
+            <button
+              onClick={onClose}
+              className={styles.cancelBtn}
+              title="Cancel habit"
+            >
+              Cancel
+            </button>
+            <button
+              className={styles.deleteBtn}
+              onClick={onConfirm}
+              title="Delete habit"
+            >
+              Delete
+            </button>
+          </div>
+        </MotionDialog>
+      )}
+    </AnimatePresence>
   );
 };
 
